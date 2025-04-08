aea2771407363314172116d175dde8fda2ccd751
Edumfg1524 <eduardomathias450@gmail.com>
2025-04-07 22:29:03 -0300

    Add files via upload

---
+++ workspace/pragram1/pragram1/Program.cs
@@ -0,0 +1,30 @@
+﻿string texto = "Eduardo Mathias";
+Console.WriteLine("Dudu");
+Console.WriteLine(texto);
+
+int a;
+int b;
+int c;
+
+a = 10;
+b = 20;
+c = a + b;
+
+Console.WriteLine(a);
+Console.WriteLine(b);
+Console.WriteLine("Resultado " + c);
+
+
+Console.WriteLine("Este é um teste de leitura de teclado");
+Console.WriteLine("Não chore");
+string s = Console.ReadLine();
+Console.WriteLine(s);
+
+string e;
+int f;
+int d;
+Console.WriteLine(15);
+e = Console.ReadLine();
+c = Convert.ToInt32(e);
+Console.WriteLine("15:");
+Console.WriteLine(c);
